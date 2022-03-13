6ba5cddf580558e511433cfb5f4662571bc56da1
Muhammad Iqbal <iqbal251196@gmail.com>
2022-03-13 15:17:52 +0700

    last

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 import moduleProject "github.com/Muhammad-Iqbal11111/moduleProject"
 
 type Strukpembayaran struct {
-	Nama string
+	Name string
 }
 type DataPelanggan struct {
 	Nama         string
@@ -12,48 +12,47 @@ type DataPelanggan struct {
 	Umur         int8
 	Pekerjaan string
 }
+
 func main(){
-	
-	moduleProject.Welcome("Muhammad Iqbal", "Y" string)
-
-	var pilihanBaju = [] string{"Kemeja Formal", "Kemeja Casual", "Kaos Krah", "Kaos Oblong"}
-	moduleProject.ItemPakaian("Muhammad Iqbal", pilihanBaju[4])
-	for _ , baju := range pilihanBaju {
-		if baju ==  "Kemeja Formal"{
-			harga := 200000
-			hargaPokok:= 120000
-			fmt.Println("Harga kemeja formal ini adalah", harga)
-		} else if baju == "Kemeja Casual"{
-			harga := 180000
-			hargaPokok:= 100000
-			fmt.Println("Harga kemeja casual ini adalah", harga)
-		} else if baju == "Kaos Krah"{
-			harga := 150000
-			hargaPokok:=70000
-			fmt.Println("Harga kaos krah ini adalah", harga)
-		} else {
-			harga := 50000
-			hargaPokok:=20000
-			fmt.Println("Harga kaos oblong ini adalah", harga)
-		}
+	moduleProject.AlamatToko()
+	moduleProject.Welcome("Muhammad Iqbal")
+	var pilihanBaju = []string{"Kemeja Formal", "Kemeja Casual", "Kaos Krah", "Kaos Oblong"}
+	moduleProject.ItemPakaian("Muhammad Iqbal", pilihanBaju...)
+	fmt.Println("Please choose the clothes you want to buy!")
+	var pilihan string
+	fmt.Scanln(&pilihan)
+	if pilihan == "Kemeja Formal"{
+		harga := 200000
+		hargaPokok:=150000
+		fmt.Println("The Price of this shirt is", harga)
 		x, y := moduleProject.ProfitSale(harga, hargaPokok)
-		fmt.Printf("Keuntungan adalah %.2f dengan persentase keuntungan %.2f percen \n", x, y
-		modulProject.Pembayaran()
-
-		struk1 := Strukpembayaran(Nama: "Muhammad Iqbal")
-		struk1.grandOpening("Berkah")
-	}
-	defer fmt.Println("terimakasih sudah berbelanja")
-
-	Pelanggan1 := DataPelanggan{
-		Nama:         "Muhammad Iqbal",
-		Alamat : "Padang",
-		Umur:         25,
-		Pekerjaan:        "Staf Operasional dan Pembayaran",
-	}
-	fmt.Println(Pelanggan1.Nama)
-	fmt.Println(Pelanggan1.Alamat)
+		fmt.Printf("The profit for the shop is %d\n", x)
+		fmt.Printf("Discount for customers is %d\n", y)
+	} else if pilihan == "Kemeja Casual"{
+		harga := 180000
+		hargaPokok:=130000
+		fmt.Println("The Price of this shirt is", harga)
+		x, y := moduleProject.ProfitSale(harga, hargaPokok)
+		fmt.Printf("The profit for the shop is %d\n", x)
+		fmt.Printf("Discount for customers is %d\n", y)
+	} else if pilihan == "Kaos Krah"{
+		harga := 120000
+		hargaPokok:=70000
+		fmt.Println("The Price of this shirt is", harga)
+		x, y := moduleProject.ProfitSale(harga, hargaPokok)
+		fmt.Printf("The profit for the shop is %d\n", x)
+		fmt.Printf("Discount for customers is %d\n", y)
+	} else {
+		harga := 70000
+		hargaPokok:=40000
+		fmt.Println("The Price of this shirt is", harga)
+		x, y := moduleProject.ProfitSale(harga, hargaPokok)
+		fmt.Printf("The profit for the shop is %d\n", x)
+		fmt.Printf("Discount for customers is %d\n", y)	}
+	
+		struk1 := Strukpembayaran{Name: "Muhammad Iqbal"}
+		struk1.GrandOpening("Berkah")
+		moduleProject.CustomerData()
+		moduleProject.selesai()
 
 }
-func (struk Strukpembayaran) grandOpening(toko string) {
-	fmt.Println("terimakasih telah berbelanja di toko ", toko, "semoga senang dengan layanan kami", struk.Nama)
